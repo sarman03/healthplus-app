adae03a4424a99e6a8cfb67469b3109881841340
sarman03 <sarmanpreets.it.22@nitj.ac.in>
2024-08-28 14:16:20 +0530

    patientform

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -3,32 +3,90 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
- 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form"
+import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
+
+export enum FormFieldType{
+  INPUT= 'input',
+  TEXTAREA= 'textarea',
+  PHONE_INPUT= 'phoneInput',
+  CHECKBOX= 'checkbox',
+  DATEPICKER= 'datePicker',
+  SELECT= 'select',
+  SKELETON= 'skeleton'
+}
+
  
 const PatientForm = () => {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+  
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
   })
  
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true)
+
+    try {
+     // const userData = { name, email, phone }
+
+     // const user = await createUser(userData)
+
+     // if(user) router.push(`/patients/${user.$id}/register`)
+    } catch (error) {
+      console.log(error)
+    }
   }
+
   return (
-    <div>
-      df
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there!</h1>
+          <p className="text-dark-700">Schedule your first appointment</p>
+        </section>
+        
+        <CustomFormField
+        fieldType= {FormFieldType.INPUT}
+        control={form.control}
+        name= 'name'
+        label= 'Full name'
+        placeholder= 'Chetan'
+        iconSrc= '/assets/icons/user.svg'
+        iconAlt= 'user' />
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="chetan@gmail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone number"
+          placeholder="(555) 123-4567"
+        />
+
+        <SubmitButton isLoading= {isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
   )
 }
 
